130d04dd6a86a3d04d8edb3eb370284bfea7de8e
yash bhange <yashbhange888@gmail.com>
2020-11-21 23:33:08 +0530

    some csss and coments

--- src/components/myHouses1.js
+++ src/components/myHouses1.js
@@ -27,7 +27,7 @@ async componentWillMount(){
 
     async loadWeb3(){
         if(window.ethereum){
-          window.web3=new Web3(window.ethereum);//new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));   //new Web3(window.ethereum);
+          window.web3=new Web3(window.ethereum);
       
          
           await window.ethereum.enable();
